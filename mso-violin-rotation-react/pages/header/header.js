cccbab946ea21df9ec1ecf1094dc102f1af97a06
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2023-09-13 15:11:32 -0500

    beginning website rebuild using react and next.js

---
+++ mso-violin-rotation-react/pages/header/header.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Is this working yet??</h1>;
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
